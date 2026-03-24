c2b1dc61522ba0951e78230bf3f659cff8de69a6
Corentin Dupriez <dupriez.c@icloud.com>
2026-03-24 15:03:27 +0200

    FEATURE: Added duckdb integration

--- dashi/datasources/registry.py
+++ dashi/datasources/registry.py
@@ -1,4 +1,5 @@
 from .csv_datasource import CsvDatasource
 from .json_datasource import JsonDatasource
+from .sql_datasource import SqlDatasource
 
-DATASOURCES = {"csv": CsvDatasource, "json": JsonDatasource}
+DATASOURCES = {"csv": CsvDatasource, "json": JsonDatasource, "duckdb": SqlDatasource}
--- dashi/datasources/sql_datasource.py
+++ dashi/datasources/sql_datasource.py
@@ -7,9 +7,9 @@ class SqlDatasource(BaseDatasource):
     def __init__(self, source_def: dict) -> None:
         super().__init__(source_def)
         self.query = source_def["query"]
-        self.path = f"{source_def['name']}.duckdb"
+        self.path = self.STAGING_DATA_PATH / f"{source_def['name']}.duckdb"
 
     def load_data(self) -> pl.DataFrame:
-        con = duckdb.connect()
-        con.execute(self.query)
-        return con.fetchall().pl()
+        con = duckdb.connect(database=self.path)
+        df = con.sql(self.query).pl()
+        return df
